60c474c4f442d6d77a0b047e014f098dab344cb8
Nagy Levente <nlevente1997@gmail.com>
2022-05-27 16:27:42 +0200

    fixed wrong filename in launch.sh

--- launch.sh
+++ launch.sh
@@ -5,5 +5,5 @@
 # @reboot sh /home/pi/Desktop/szakdolgozat/launch.sh >/home/pi/Desktop/szakdolgozat/logs/robot.log 2>&1
 
 cd /home/pi/Desktop/szakdolgozat/
-python3 -u /home/pi/Desktop/szakdolgozat/carcontroller.py
+python3 -u /home/pi/Desktop/szakdolgozat/robotcontroller.py
 cd /
